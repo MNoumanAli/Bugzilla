de6ce7e8a1ab0d4222e65c1968c8240e4d9dcd9b
MNoumanAli <mnoumanali001@gmail.com>
2023-10-12 23:52:07 +0500

    add schema validation middleware

---
+++ server/middlewares/validateSchema.js
@@ -0,0 +1,11 @@
+const validateSchema = (schema) => {
+  return async function (req, res, next) {
+    try {
+      await schema.validate(req.body);
+      next();
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  };
+};
+export default validateSchema;
