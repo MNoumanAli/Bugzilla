e11faa83cc7d1e3c310e0e8d7955f0b8663a2880
MNoumanAli <mnoumanali001@gmail.com>
2023-11-11 23:38:31 +0500

    putting it all togethor

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-// starting server side code
+import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import router from "./routes/index.js";
+import { connection } from "./config/db.js";
+
+export const app = express();
+dotenv.config()
+// routes
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+// define routes
+app.use("/", router.userRouter);
+app.use("/project", router.projectRouter);
+// create connection
+connection();
