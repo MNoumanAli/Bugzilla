2e70231215baadb7cae61da25f69950bd1bdb279
MNoumanAli <mnoumanali001@gmail.com>
2023-11-19 01:39:49 +0500

    add icons

---
+++ client/src/constants.js
@@ -0,0 +1,24 @@
+export const ADD_PROJECT = "ADD_PROJECT";
+export const DELETE_PROJECT = "DELETE_PROJECT";
+export const ADD_NEW_PROJECT = "ADD_NEW_PROJECT";
+export const EDIT_PROJECT = "EDIT_PROJECT";
+export const ADD_DEVELOPER = "ADD_DEVELOPER";
+export const ADD_QA = "QDD_QA";
+export const ADD_BUG = "ADD_BUG";
+export const ADD_NEW_BUG = "ADD_NEW_BUG";
+export const UPDATE_BUG_STATUS = "UPDATE_BUG_STATUS";
+export const BUG_STATUS = [
+  { value: "new", label: "new" },
+  { value: "started", label: "started" },
+  { value: "resolved", label: "resolved" },
+];
+export const FEATURE_STATUS = [
+  { value: "new", label: "new" },
+  { value: "started", label: "started" },
+  { value: "completed", label: "completed" },
+];
+export const ROLE_OPTIONS = [
+  { value: "Developer", label: "Developer" },
+  { value: "Manager", label: "Manager" },
+  { value: "QA", label: "QA" },
+];
